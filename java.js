45602cd54a6881746541a761842e29357310d3b9
Andrew Spadaro <andrewspadaro@gmail.com>
2020-10-31 12:14:33 -0400

    sudo code

---
+++ java.js
@@ -0,0 +1,10 @@
+//create an opening page to the quiz with a button to start quiz
+//create a timer that starts at 30 seconds 
+//when button is clicked make first question to the quiz appear
+//when answer is clicked make question dissaper
+//figure out how to take 5 seconds off timer for every incorrect answer
+//create a high score page with prompt to enter initials
+//store scores to local computer
+
+
+
